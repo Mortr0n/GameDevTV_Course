a8789791def3e6c9b04c85b28a1d884b40fbf7df
Mortron <cmsl4y3r@gmail.com>
2024-05-09 20:22:37 -0600

    used visibleOnScreenNotifier to detect screen exit and connected to rocket script to remove on exit

--- AlienAttack/scripts/rocket.gd
+++ AlienAttack/scripts/rocket.gd
@@ -2,5 +2,15 @@ extends Area2D
 
 @export var rocket_speed = 300
 
+@onready var visible_notifier: VisibleOnScreenNotifier2D = $VisibleNotifier
+
+
+func _ready() -> void:
+	visible_notifier.connect("screen_exited", _on_screen_exited)
+
 func _physics_process(delta: float) -> void:
 	global_position.x += rocket_speed * delta
+
+func _on_screen_exited():
+	#print("Rockets Red Glare")
+	queue_free()
