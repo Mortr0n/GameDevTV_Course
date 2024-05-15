107e8ee99d1d004506e749d775739c60a1c045a4
Mortron <cmsl4y3r@gmail.com>
2024-05-14 19:32:45 -0600

    added deathzone area and set mask and layers accordingly

--- AlienAttack/scripts/BasicEnemy.gd
+++ AlienAttack/scripts/BasicEnemy.gd
@@ -8,9 +8,12 @@ func _physics_process(delta: float) -> void:
 	global_position.x -= move_speed * delta
 
 
+# this became invalid when they decided to make a collision shape and use it to queue_free the ship.
+# I'm not sure on the pro's and cons for either way
 # did this by clicking in the node signals, but you can also code these up.  See Rocket code for hand coded one
-func _on_visible_notifier_screen_exited() -> void:
-	queue_free()
+#func _on_visible_notifier_screen_exited() -> void:
+	##queue_free()
+	#pass
 	
 func die() -> void:
 	print("dying!")
