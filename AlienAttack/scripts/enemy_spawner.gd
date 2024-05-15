b4981236e9680f68e77958a6759c535ccdc71bdd
Mortron <cmsl4y3r@gmail.com>
2024-05-14 18:28:27 -0600

    Their way of doing it with my final commented out

--- AlienAttack/scripts/enemy_spawner.gd
+++ AlienAttack/scripts/enemy_spawner.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-var enemy = preload("res://scenes/basic_enemy.tscn")
+var enemy_scene = preload("res://scenes/basic_enemy.tscn")
+@onready var spawn_positions: Node2D = $SpawnPositions
+
 
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
 
 func spawn_enemy():
-	var enemy_instance = enemy.instantiate()
-	var spawn_pos_parent = self.get_children()[0]
-	#var first_child = self.
-	var spawners = spawn_pos_parent.get_children()
-
-	var rng = RandomNumberGenerator.new()
-	var r_num = rng.randi_range(0,5)
-	spawners[r_num].add_child(enemy_instance)
+	var enemy_instance = enemy_scene.instantiate()
+	var random_spawner = spawn_positions.pick_random()
+	
+	enemy_instance.global_position = random_spawner.global_position
+	add_child(enemy_instance)
+# I'm not sure that there's is better than my way, but there's more ways than one to do it.
+	#var spawners = spawn_positions.get_children()
+	#var rng = RandomNumberGenerator.new()
+	#var r_num = rng.randi_range(0,5)
+	#spawners[r_num].add_child(enemy_instance)
